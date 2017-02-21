dd603c0e4e58068dd753846b8cd295645d53c052
Matheus Vilvert <matheusvilvert@gmail.com>
2017-02-20 22:17:11 -0400

    alteracoes

--- sistema-usuarios/src/main/java/controller/UsuarioController.java
+++ sistema-usuarios/src/main/java/controller/UsuarioController.java
@@ -3,10 +3,12 @@ package controller;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet(urlPatterns = "/usucontroller")
 public class UsuarioController extends HttpServlet{
 
 	
