05867da1ba257fe8d6be2748d194de807e9955e5
MatheusVilvert <matheusvilvert@gmail.com>
2017-03-06 23:03:55 -0300

    JSon

--- sistema-usuarios/src/main/java/controller/UsuarioController.java
+++ sistema-usuarios/src/main/java/controller/UsuarioController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,18 +10,43 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.Usuario;
+
 @WebServlet(urlPatterns = "/usucontroller")
 public class UsuarioController extends HttpServlet{
 
 	
 	private static final long serialVersionUID = 1L;
 	
+	private List<Usuario> lista = new ArrayList<>();
+	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		//capturando oq vem do client/navegador
+		String nome = req.getParameter("nome");
 		String email = req.getParameter("email");
-		//respondendo
-		resp.getWriter().print("ola "+email);
+		//instanciando obj
+		Usuario usu = new Usuario(nome,email);
+		//inserinfo na lista
+		lista.add(usu);
+		
+		resp.getWriter().println("{ nome:" + usu.getNome() + "  , email: " + usu.getEmail() + "  } ");
+		
+		
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String json = "[";
+		for (int i = 0; i < lista.size(); i++) {
+
+			json += "{ nome:" + lista.get(i).getNome() + "  , email: " + lista.get(i).getEmail() + "  }";
+			if (i < lista.size()-1)
+				json += ",";
+		}
+		json += "]";
+
+		resp.getWriter().print(json);
 	}
 
 }
