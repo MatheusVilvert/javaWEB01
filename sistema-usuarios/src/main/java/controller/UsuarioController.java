139614e4c298735c013557fe068114b5cf663a92
MatheusVilvert <matheusvilvert@gmail.com>
2017-03-13 22:15:57 -0400

    altera e exclui

--- sistema-usuarios/src/main/java/controller/UsuarioController.java
+++ sistema-usuarios/src/main/java/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,15 +26,52 @@ public class UsuarioController extends HttpServlet{
 		//capturando oq vem do client/navegador
 		String nome = req.getParameter("nome");
 		String email = req.getParameter("email");
-		//instanciando obj
+		//instanciando obj		
 		Usuario usu = new Usuario(nome,email);
 		//inserinfo na lista
-		lista.add(usu);
+		
+				lista.add(usu);
+			
+		
 		
 		resp.getWriter().println("{ nome:" + usu.getNome() + "  , email: " + usu.getEmail() + "  } ");
 		
 		
 	}
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//deletar por nome+email
+		/*String nome = req.getParameter("nome");
+		String email = req.getParameter("email");		
+		for (int i = 0; i < lista.size(); i++) {
+			if(lista.get(i).getEmail().equals(email) && lista.get(i).getNome().equals(nome) ){
+				lista.remove(i);
+				i = lista.size();
+			}
+		}*/
+		
+		//deletar por indice
+		int indice = Integer.parseInt(req.getParameter("indice"));		
+		lista.remove(indice);		
+	}
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int indice = Integer.parseInt(req.getParameter("indice"));
+		
+		String nome = req.getParameter("nome");
+		String email = req.getParameter("email");
+		
+		if(nome != "" && email.equals("")){
+			lista.get(indice).setNome(nome);
+		}else if(nome.equals("") && email != ""){
+			lista.get(indice).setEmail(email);
+		}else if(nome != "" && email != ""){
+			lista.get(indice).setNome(nome);
+			lista.get(indice).setEmail(email);
+		}
+		
+	}
+	
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
